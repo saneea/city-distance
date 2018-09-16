7f02bcc6dabd93c33a848186ea6247a0462bf50f
Alexander Sokol <saneea@bk.ru>
2018-09-16 11:01:25 +0300

    add tests

--- src/test/java/io/github/saneea/citydistance/DistanceControllerIT.java
+++ src/test/java/io/github/saneea/citydistance/DistanceControllerIT.java
@@ -37,18 +37,87 @@ public class DistanceControllerIT {
 	}
 
 	@Test
-	public void simpleTest() throws Exception {
-		assertReflectionEquals(null, post("A", "B", 20));
+	public void noArgumentTest() throws Exception {
+		assertReflectionEquals(//
+				error(ErrorCode.ARGUMENT_NOT_SPECIFIED, "Argument \"city1\" was not specified"), //
+				post(null, "B", 20));
 
 		assertReflectionEquals(//
-				error(ErrorCode.REDEFINING_DISTANCE, "Distance between \"A\" and \"B\" has been already defined"), //
-				post("A", "B", 20));
+				error(ErrorCode.ARGUMENT_NOT_SPECIFIED, "Argument \"city2\" was not specified"), //
+				post("A", null, 20));
 
-		assertReflectionEquals(response("A", "B", 20), get("A", "B"));
+		assertReflectionEquals(//
+				error(ErrorCode.ARGUMENT_NOT_SPECIFIED, "Argument \"distance\" was not specified"), //
+				post("A", "B", null));
 	}
 
-	private Response get(String city1, String city2) {
-		return template.getForEntity(base + "/" + city1 + "/" + city2, Response.class).getBody();
+	@Test
+	public void badArgumentTest() throws Exception {
+		assertReflectionEquals(//
+				error(ErrorCode.BAD_ARGUMENT, "Argument \"city1\" is invalid. It must be non-empty-string"), //
+				post("", "B", 20));
+
+		assertReflectionEquals(//
+				error(ErrorCode.BAD_ARGUMENT, "Argument \"city2\" is invalid. It must be non-empty-string"), //
+				post("A", "", 20));
+
+		assertReflectionEquals(//
+				error(ErrorCode.BAD_ARGUMENT, "Argument \"distance\" is invalid. It must be positive number"), //
+				post("A", "B", 0));
+
+		assertReflectionEquals(//
+				error(ErrorCode.BAD_ARGUMENT, "Argument \"distance\" is invalid. It must be positive number"), //
+				post("A", "B", -1));
+	}
+
+	@Test
+	public void noCityTest() throws Exception {
+		assertReflectionEquals(null, post("C", "D", 20));
+
+		assertReflectionEquals(error(ErrorCode.CITY_NOT_FOUND, "City \"N1\" was not found"), getForError("N1", "D"));
+		assertReflectionEquals(error(ErrorCode.CITY_NOT_FOUND, "City \"N2\" was not found"), getForError("C", "N2"));
+	}
+
+	@Test
+	public void positiveTest() throws Exception {
+		assertReflectionEquals(null, post("E", "F", 20));
+		assertReflectionEquals(response("E", "F", 20), getForOk("E", "F"));
+
+		assertReflectionEquals(//
+				error(ErrorCode.REDEFINING_DISTANCE, "Distance between \"E\" and \"F\" has been already defined"), //
+				post("E", "F", 20));
+	}
+
+	@Test
+	public void redefineDistanceTest() throws Exception {
+		assertReflectionEquals(null, post("G", "H", 20));
+		assertReflectionEquals(response("G", "H", 20), getForOk("G", "H"));
+
+		assertReflectionEquals(//
+				error(ErrorCode.REDEFINING_DISTANCE, "Distance between \"G\" and \"H\" has been already defined"), //
+				post("G", "H", 20));
+	}
+
+	@Test
+	public void noPathTest() throws Exception {
+		assertReflectionEquals(null, post("I", "J", 20));
+		assertReflectionEquals(null, post("K", "L", 20));
+
+		assertReflectionEquals(//
+				error(ErrorCode.NO_PATH, "Path between \"I\" and \"L\" can not be built"), //
+				getForError("I", "L"));
+	}
+
+	private Response getForOk(String city1, String city2) {
+		return getFor(city1, city2, Response.class);
+	}
+
+	private ErrorEntity getForError(String city1, String city2) {
+		return getFor(city1, city2, ErrorEntity.class);
+	}
+
+	private <ResponseType> ResponseType getFor(String city1, String city2, Class<ResponseType> responseType) {
+		return template.getForEntity(base + "/" + city1 + "/" + city2, responseType).getBody();
 	}
 
 	private ErrorEntity post(String city1, String city2, Integer distance) {
